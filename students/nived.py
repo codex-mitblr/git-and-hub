6a167188baa48f9c2f1daadb69e68cdf1ae7dbe9
nived <63636251+NNNiv@users.noreply.github.com>
2024-01-24 18:44:34 +0530

    grammar *thumbs up emoji*

--- students/nived.py
+++ students/nived.py
@@ -3,8 +3,8 @@ print(
       If Abhigyan had a million fans, I am one of them
       If Abhigyan had a thousand fans, I am one of them
       If Abhigyan had a hundred fans, I am one of them
-      If Abhigyan had a ten fans, I am one of them
-      If Abhigyan had a one fan, I am one of them
+      If Abhigyan had ten fans, I am one of them
+      If Abhigyan had one fan, I am one of them
       If Abhigyan had no fans, I am dead
       """
 )
